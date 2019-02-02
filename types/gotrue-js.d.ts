72ce4175ccb2323ba7772b26aa42a7d97d446156
Rendall <rendall@gmail.com>
2019-02-02 16:54:18 +0200

    add user management html

---
+++ types/gotrue-js.d.ts
@@ -0,0 +1,18 @@
+export default class _default {
+  constructor(...args: any[]);
+  audience: any;
+  setCookie: any;
+  api: any;
+  acceptInvite(token: any, password: any, remember: any): any;
+  acceptInviteExternalUrl(provider: any, token: any): any;
+  confirm(token: any, remember: any): any;
+  createUser(tokenResponse: any, ...args: any[]): any;
+  currentUser(): any;
+  login(email: any, password: any, remember: any): any;
+  loginExternalUrl(provider: any): any;
+  recover(token: any, remember: any): any;
+  requestPasswordRecovery(email: any): any;
+  settings(): any;
+  signup(email: any, password: any, data: any): any;
+  verify(type: any, token: any, remember: any): any;
+}
