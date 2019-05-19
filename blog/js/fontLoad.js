0ba2abf681a4e1c323240aedfb3d1e60ffa511e2
Rendall <rendall@gmail.com>
2019-05-19 11:23:33 +0300

    Remove 'fontLoad' code

--- blog/js/fontLoad.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var FontFaceObserver = require('fontfaceobserver');
-exports.onFontLoad = function (fontName, onLoad, onError) { return new FontFaceObserver(fontName).load().then(onLoad)["catch"](onError); };
