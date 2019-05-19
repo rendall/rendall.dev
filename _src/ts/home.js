f178ef1f78b1d5a1d8e58bfac52c2c1c3a639e8a
Rendall <rendall@gmail.com>
2019-05-19 11:31:01 +0300

    Clean up build files

--- _src/ts/home.js
+++
@@ -1,4 +0,0 @@
-import { setupCommentForm } from "./commentForm";
-import { onFontLoad } from "./fontLoad";
-onFontLoad("Montserrat", function () { document.querySelector("html").classList.add("web-font"); });
-setupCommentForm();
