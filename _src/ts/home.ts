e60d2454d3779aa36fe2f495977bfe9aaeeaf22a
Rendall <rendall@gmail.com>
2019-07-07 19:52:31 +0300

    Remove fontLoad code

--- _src/ts/home.ts
+++ _src/ts/home.ts
@@ -1,5 +1,2 @@
 import { setupCommentForm } from "./commentForm";
-import { onFontLoad } from "./fontLoad";
-
-onFontLoad("Montserrat", () => { document.querySelector("html")!.classList.add("web-font") } )
 setupCommentForm();
