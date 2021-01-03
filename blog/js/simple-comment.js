6c505c7be37892db2257257f3fa988cbec81002b
Rendall <rendall@gmail.com>
2021-01-03 17:49:18 +0200

    Add simple-comment to blog base

--- blog/js/simple-comment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-console.log("simple-comment");
-
-fetch("https://blog-rendall-dev-comments.netlify.app/.netlify/functions/auth")
-.then(response => {
-  console.log({response})
-})
-.catch(error => console.error({error}))
