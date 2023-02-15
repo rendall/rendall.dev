0b2d7a36bedd512c6aa0b4e0fbe7f5c0cb772e17
Rendall <rendall@gmail.com>
2023-02-15 12:29:53 +0200

    Format local javascript

--- _src/blog/_11ty/getTagList.js
+++ _src/blog/_11ty/getTagList.js
@@ -1,10 +1,10 @@
 module.exports = function (collection) {
-  let tagSet = new Set();
+  let tagSet = new Set()
   collection.getAllSorted().forEach(function (item) {
     if ("tags" in item.data) {
-      let tags = item.data.tags;
+      let tags = item.data.tags
       if (typeof tags === "string") {
-        tags = [tags];
+        tags = [tags]
       }
 
       tags = tags.filter(function (item) {
@@ -14,18 +14,18 @@ module.exports = function (collection) {
           case "nav":
           case "post":
           case "posts":
-            return false;
+            return false
         }
 
-        return true;
-      });
+        return true
+      })
 
       for (const tag of tags) {
-        tagSet.add(tag);
+        tagSet.add(tag)
       }
     }
-  });
+  })
 
   // returning an array in addCollection works in Eleventy 0.5.3
-  return [...tagSet];
-};
+  return [...tagSet]
+}
