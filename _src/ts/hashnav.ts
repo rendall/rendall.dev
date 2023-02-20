17faae66f097035f403e521d5fe2ddcc1e022331
Rendall <rendall@gmail.com>
2023-02-20 14:55:34 +0200

    Add active class to link when pressed

--- _src/ts/hashnav.ts
+++ _src/ts/hashnav.ts
@@ -1,14 +1,14 @@
 // Add 'active' class to the correct anchor when #hash is in the url
-window.addEventListener("hashchange", () => {
+const updateActive = () => {
   const anchors = document.querySelectorAll(".Site__Side a")
+  anchors.forEach((a) => a.classList.remove("active"))
   const hash = window.location.hash
-  if (hash && hash.length > 1) {
-    anchors.forEach((a) => a.classList.remove("active"))
-    anchors.forEach((a) => {
-      if (a.getAttribute("href") === hash) a.classList.add("active")
-    })
-  }
-})
+  if (hash && hash.length > 1) anchors.forEach((a) => {
+    if (a.getAttribute("href") === window.location.hash)
+      a.classList.add("active")
+  })
+  
+}
 
 // Add 'active' class to the anchor when clicked
 // (otherwise there is a slight delay before it changes)
@@ -19,3 +19,7 @@ document.querySelectorAll(".Site__Side a").forEach((a, _i, all) => {
     )
   a.addEventListener("click", addActive(a, all))
 })
+
+window.addEventListener("hashchange", updateActive)
+
+updateActive()
